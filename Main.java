0afb93c92d7072c702cb5b6da835e99513076459
jack-rabe <76982748+jack-rabe@users.noreply.github.com>
2021-04-30 13:55:28 -0500

    added rules scene

--- Main.java
+++ Main.java
@@ -11,28 +11,27 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 public class Main extends Application {
-  private BorderPane mainPane;
+  private Stage stage;
   private final int WIDTH = 600, HEIGHT = 600;
   private Scene menuScreen, rulesScreen, gameScreen;
 
   private Scene createMenuScreen() {
+    BorderPane mainPane = new BorderPane();
+    mainPane.setStyle("-fx-background-color: blue;");
     Scene scene = new Scene(mainPane, WIDTH, HEIGHT);
+    scene.getStylesheets().add("menuStyles.css");
     
     // Display the title of the game top and center
-    Label title = new Label("Jack's Typing Game");
+    Label title = new Label("Home");
     title.setFont(Font.font(28));
     BorderPane.setAlignment(title, Pos.CENTER);
     mainPane.setTop(title);
 
     // create the start, rules, and quit buttons
     Button startBtn = new Button("Start");
-    startBtn.setOnAction(event -> {
-      //TODO add scene change
-    });
+    startBtn.setOnAction(event -> { stage.setScene(gameScreen); });
     Button rulesBtn = new Button("Rules");
-    rulesBtn.setOnAction(event -> {
-      //TODO add scene change
-    });
+    rulesBtn.setOnAction(event -> { stage.setScene(rulesScreen); });
     Button quitBtn = new Button("Quit");
     quitBtn.setOnAction(event -> { Platform.exit(); });
     
@@ -43,18 +42,41 @@ public class Main extends Application {
     btnBox.getChildren().add(startBtn);
     btnBox.getChildren().add(rulesBtn);
     btnBox.getChildren().add(quitBtn);
-    // add the button styles
-    scene.getStylesheets().add("Styles.css");
     
     BorderPane.setAlignment(btnBox, Pos.CENTER);
     mainPane.setCenter(btnBox);
-    
     return scene;
   }
 
   // TODO make a rules scene
   private Scene createRulesScreen() {
-    return null;
+    BorderPane mainPane = new BorderPane();
+    Scene scene = new Scene(mainPane, WIDTH, HEIGHT);
+    
+    // Display the title of the game top and center
+    Label title = new Label("Rules");
+    title.setFont(Font.font(28));
+    BorderPane.setAlignment(title, Pos.CENTER);
+    mainPane.setTop(title);
+
+    // create the start, rules, and quit buttons
+    Button menuBtn = new Button("Menu");
+    menuBtn.setOnAction(event -> { stage.setScene(menuScreen); });
+    Button quitBtn = new Button("Quit");
+    quitBtn.setOnAction(event -> { Platform.exit(); });
+    
+    // add all buttons to a VBox
+    VBox btnBox = new VBox();
+    btnBox.setSpacing(20);
+    btnBox.setAlignment(Pos.CENTER);
+    btnBox.getChildren().add(menuBtn);
+    btnBox.getChildren().add(quitBtn);
+    // add the button styles
+    scene.getStylesheets().add("ruleStyles.css");
+    
+    BorderPane.setAlignment(btnBox, Pos.CENTER);
+    mainPane.setCenter(btnBox);
+    return scene;
   }
   
   // TODO: make a game scene
@@ -64,7 +86,7 @@ public class Main extends Application {
 
   @Override
   public void start(final Stage stage) {
-    mainPane = new BorderPane();
+    this.stage = stage;
     
     menuScreen = createMenuScreen();
     rulesScreen = createRulesScreen();
